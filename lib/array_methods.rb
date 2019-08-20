8fbfdafe217c5f17a0ccc82e2d59b5b82f08e2e3
Matt Plichta <bigcatplichta@gmail.com>
2019-08-20 00:01:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort
-  puts array(-1)
+  array.sort = sorted_array
+  puts sorted_array(-1)
 end
 
 def find_min_value(array)
